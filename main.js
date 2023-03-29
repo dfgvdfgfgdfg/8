8d3841a1ba77f668b500fab6718ba1c789ce9ce1
Aarush Chaudhary <86662135+dfgvdfgfgdfg@users.noreply.github.com>
2023-03-29 17:29:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,550);
+    video.position(150,150)
+
+    canvas=createCanvas(550, 550);
+    canvas.position(700,150);
+
+    poseNet =ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function draw(){
+    background('#7ED08A')
+}
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!');
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
+
